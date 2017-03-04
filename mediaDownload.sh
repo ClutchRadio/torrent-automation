f81ca1116b2e793b441bf6aeb2dca34be733823d
Nate Loiselle <Nate Loiselle>
2017-03-04 13:05:33 -0500

    Added Media Download Script

---
+++ mediaDownload.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+PROTOCOL="sftp"
+URL="riolu.tal0ne.co.uk"
+PORT="21976"
+LOCALDIR="/home/ubuntu/Downloads/"
+REMOTEDIR="/rtorrent/downloads/"
+USER="nervlabs"
+PASS="g7Ml4J9fo5"
+REGEX="*"
+LOG="/home/ubuntu/Scripts/logs/mediaDownload.log"
+
+cd $LOCALDIR
+if [  $? -dt 0 ]; then
+    echo "$(date "+%d/%m/%Y-%T") Cant cd to $LOCALDIR. Please make sure this local directory is valid" >> $LOG
+fi
+
+lftp  -p $PORT $PROTOCOL://$URL <<- DOWNLOAD
+    user $USER "$PASS"
+    cd $REMOTEDIR
+    mget -E $REGEX
+DOWNLOAD
+
+if [  $? -dt 0 ]; then
+    echo "$(date "+%d/%m/%Y-%T") Cant download files. Make sure the credentials and server information are correct" >> $LOG
+fi
