ecbf5a183d1949ff34c68f3beb0dd01b01b5cbab
Nate Loiselle <Nate.loiselle@gmail.com>
2017-03-05 15:32:35 -0500

    WRiting part to find if download has a rar to unpack

--- unpackDownloads.sh
+++ unpackDownloads.sh
@@ -3,4 +3,9 @@
 SOURCEDIR="/media/Data/TempDownloads/"
 
 #For each folder in TempDownloads
-for folder in $(find "$SOURCEDIR" -maxdepth 1)
+for folder in $(find "$SOURCEDIR" -maxdepth 1) do
+  echo "$folder"
+  if (find "$folder" -maxdepth 1 -name "*.rar") == TRUE do
+    echo "Found a Rar"
+  done
+done
