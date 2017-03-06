619a2232fefdb841d5d6c1038909dd86ee7cdb31
Nate Loiselle <Nate.loiselle@gmail.com>
2017-03-06 10:04:13 -0500

    commit

--- unpackDownloads.sh
+++ unpackDownloads.sh
@@ -5,7 +5,7 @@ SOURCEDIR="/media/Data/TempDownloads/"
 #For each folder in TempDownloads
 for folder in $(find "$SOURCEDIR" -maxdepth 1); do
   echo "$folder"
-  if [[ $(find "$folder" -maxdepth 1 -name "*.rar" | wc -l) -gt 0 ]]; then
+  if [[ $(find "$folder" -maxdepth 1 -name "*.rar" -print | wc -l) -gt 0 ]]; then
     echo "Found a Rar"
   fi
 done
